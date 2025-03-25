e96e1c3971ec4c5871ccfa678b87fc002f819f5a
furkanh7 <furkanhilaloglu9@gmail.com>
2025-03-25 21:59:26 +0300

    Add project files.

---
+++ Blog.Entity/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Core.Entities;
+
+namespace Blog.Entity.Entities
+{
+    public class Category : EntityBase, IEntityBase
+    {
+      
+
+        public string Name { get; set; }
+
+        public ICollection<Article> Articles { get; set; }
+
+    }
+}

00597b1890b508831690edfe3697f14d9ca98b39
furkanh7 <furkanhilaloglu9@gmail.com>
2025-03-31 21:29:37 +0300

    add to UnitOfWorks

--- Blog.Data/Extensions/DataLayerExtensions.cs
+++ Blog.Data/Extensions/DataLayerExtensions.cs
@@ -1,12 +1,10 @@
-﻿using Blog.Data.Repositories.Abstractions;
+﻿using Blog.Data.Context;
+using Blog.Data.Repositories.Abstractions;
 using Blog.Data.Repositories.Concretes;
+using Blog.Data.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.Data.Extensions
 {
@@ -15,6 +13,9 @@ namespace Blog.Data.Extensions
         public static IServiceCollection LoadDataLayerExtensions(this IServiceCollection services, IConfiguration config)
         {
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             return services;
         }
 
--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -10,7 +10,6 @@ builder.Services.LoadDataLayerExtensions(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();

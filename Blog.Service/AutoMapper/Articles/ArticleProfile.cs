54606b1a41f764d36b6c7a73d0e4aa331cac948e
furkanh7 <furkanhilaloglu9@gmail.com>
2025-04-03 23:52:45 +0300

    .

--- Blog.Service/AutoMapper/Articles/ArticleProfile.cs
+++ Blog.Service/AutoMapper/Articles/ArticleProfile.cs
@@ -1,11 +1,6 @@
 ﻿using AutoMapper;
 using Blog.Entity.DTOs.Articles;
 using Blog.Entity.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Blog.Service.AutoMapper.Articles
 {

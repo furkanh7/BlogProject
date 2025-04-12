cbe97f6acbe77eb272b9b740475147cc72660e13
furkanh7 <furkanhilaloglu9@gmail.com>
2025-04-12 18:25:28 +0300

    admin theme editing

--- Blog.Data/Mappings/UserLoginMap.cs
+++ Blog.Data/Mappings/UserLoginMap.cs
@@ -18,8 +18,8 @@ namespace Blog.Data.Mappings
             builder.HasKey(l => new { l.LoginProvider, l.ProviderKey });
 
             // Limit the size of the composite key columns due to common DB restrictions
-            builder.Property(l => l.LoginProvider).HasMaxLength(127);
-            builder.Property(l => l.ProviderKey).HasMaxLength(127);
+            builder.Property(l => l.LoginProvider).HasMaxLength(128);
+            builder.Property(l => l.ProviderKey).HasMaxLength(128);
 
             // Maps to the AspNetUserLogins table
             builder.ToTable("AspNetUserLogins");
